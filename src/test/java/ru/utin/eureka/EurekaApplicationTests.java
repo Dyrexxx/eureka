9ceb2ef967700299bf4689bb744b1673bff1299f
dyrex <Dimut280501@gmail.com>
2024-08-30 15:06:10 +0300

    Initial commit

---
+++ src/test/java/ru/utin/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.utin.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
